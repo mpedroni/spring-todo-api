1251174e8ab3acfb8f8524ce9a97ce4d4cedd217
Matheus Pedroni <mpedroni77@gmail.com>
2023-05-04 00:48:41 -0300

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-todo-api
+
+To-do list API implementation using Java 17 & Spring Boot 3.0
---
+++ src/test/java/com/mpedroni/springtodoapi/SpringTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpedroni.springtodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
